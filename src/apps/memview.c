91633a878c709d5057ced47c5f26038dc39c5b9d
Mikhail Yudin <fagci.nsk@gmail.com>
2024-04-13 19:04:31 +0700

    progress

--- src/apps/memview.c
+++ src/apps/memview.c
@@ -6,32 +6,37 @@
 #include "../ui/graphics.h"
 #include "apps.h"
 
-void MEMVIEW_Init(void) {}
-
-void MEMVIEW_Update(void) { gRedrawScreen = true; }
-
 static uint32_t page = 0;
-const uint8_t PAGE_SZ = 64;
+static const uint8_t PAGE_SZ = 64;
+static uint16_t pagesCount;
+
+void MEMVIEW_Init(void) { pagesCount = SETTINGS_GetEEPROMSize() / PAGE_SZ; }
+
+void MEMVIEW_Update(void) {}
 
 void MEMVIEW_Render(void) {
-  UI_ClearScreen();
   uint8_t buf[64] = {0};
   EEPROM_ReadBuffer(page * PAGE_SZ, buf, PAGE_SZ);
 
+  UI_ClearScreen();
   for (uint8_t i = 0; i < PAGE_SZ; ++i) {
     uint32_t offset = i + page * PAGE_SZ;
+    uint8_t col = i % 8;
+    uint8_t row = i / 8;
+    uint8_t rowYBL = row * 6 + 8 + 5;
+
     if (i % 8 == 0) {
-      PrintSmall(0, (i / 8) * 6 + 8 + 5, "%u", page * PAGE_SZ + i);
+      PrintSmall(0, rowYBL, "%u", page * PAGE_SZ + i);
     }
 
-    PrintSmall(16 + (i % 8) * 9, (i / 8) * 6 + 8 + 5, "%02x", buf[i]);
-    PrintSmall(88 + (i % 8) * 5, (i / 8) * 6 + 8 + 5, "%c",
+    PrintSmall(16 + col * 9, rowYBL, "%02x", buf[i]);
+    PrintSmall(88 + col * 5, rowYBL, "%c",
                buf[i] >= 32 && buf[i] < 128 ? buf[i] : '.');
     if (offset == SETTINGS_OFFSET || offset == VFOS_OFFSET ||
         offset == PRESETS_OFFSET ||
         (offset >= PRESETS_OFFSET &&
          (offset - PRESETS_OFFSET) % PRESET_SIZE == 0)) {
-      FillRect(16 + (i % 8) * 9 - 1, (i / 8) * 6 + 8, 9, 7, C_INVERT);
+      FillRect(16 + col * 9 - 1, row * 6 + 8, 9, 7, C_INVERT);
     }
   }
 }
@@ -42,10 +47,10 @@ bool MEMVIEW_key(KEY_Code_t k, bool p, bool h) {
     APPS_exit();
     return true;
   case KEY_UP:
-    IncDec32(&page, 0, SETTINGS_GetEEPROMSize() / PAGE_SZ, -1);
+    IncDec32(&page, 0, pagesCount, -1);
     return true;
   case KEY_DOWN:
-    IncDec32(&page, 0, SETTINGS_GetEEPROMSize() / PAGE_SZ, 1);
+    IncDec32(&page, 0, pagesCount, 1);
     return true;
   case KEY_MENU:
     return false;
