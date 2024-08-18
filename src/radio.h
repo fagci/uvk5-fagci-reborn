3cb26686ca74a1e9b6e0e170b17162a5564ef05a
Mikhail Yudin <fagci.nsk@gmail.com>
2024-08-18 20:51:44 +0700

    refactor

--- src/radio.h
+++ src/radio.h
@@ -58,7 +58,6 @@ void RADIO_TuneTo(uint32_t f);
 bool RADIO_TuneToCH(int32_t num);
 void RADIO_TuneToPure(uint32_t f, bool precise);
 void RADIO_TuneToSave(uint32_t f);
-void RADIO_SelectPreset(int8_t num);
 void RADIO_SelectPresetSave(int8_t num);
 void RADIO_SetupBandParams();
 
