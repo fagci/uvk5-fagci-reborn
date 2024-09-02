fc65decf02ae8ac65dda2ad59c2f611f53186b75
Mikhail Yudin <fagci.nsk@gmail.com>
2024-09-02 10:02:34 +0700

    refactor, experiments

--- src/svc_fastscan.c
+++ src/svc_fastscan.c
@@ -16,6 +16,8 @@ static bool lower = true;
 
 static uint32_t lastSwitch = 0;
 
+static bool scanning = false;
+
 static uint32_t delta(uint32_t f1, uint32_t f2) {
   if (f1 > f2) {
     return f1 - f2;
@@ -25,9 +27,9 @@ static uint32_t delta(uint32_t f1, uint32_t f2) {
 
 void SVC_FC_Init(void) {
   SVC_Toggle(SVC_LISTEN, false, 10);
-  // BK4819_DisableFilter();
   BK4819_StopScan();
   BK4819_EnableFrequencyScanEx(T);
+  scanning = true;
   hits = 0;
 }
 
@@ -41,22 +43,28 @@ static void gotF(uint32_t f) {
     f -= sd;
   }
 
-  Loot *loot = LOOT_Add(f);
-  RADIO_TuneToPure(f, true);
-  RADIO_UpdateMeasurementsEx(loot);
+  Loot msm = {.f = f, .open = true};
+  SVC_FC_Deinit();
+  BK4819_RX_TurnOn();
+  RADIO_TuneTo(f);
+  // RADIO_ToggleRX(true);
+  LOOT_Update(&msm);
 }
 
 void SVC_FC_Update(void) {
   uint32_t f = 0;
+  if (gIsListening ||
+      (gLastActiveLoot && Now() - gLastActiveLoot->lastTimeOpen < 2000)) {
+    return;
+  }
 
   if (Now() - lastSwitch >= 1000) {
     lastSwitch = Now();
-    // Log("switch:%u", lower);
     lower = !lower;
     BK4819_SelectFilter(lower ? 14500000 : 43300000);
   }
 
-  if (!BK4819_GetFrequencyScanResult(&f)) {
+  if (scanning && !BK4819_GetFrequencyScanResult(&f)) {
     return;
   }
 
@@ -67,7 +75,7 @@ void SVC_FC_Update(void) {
       gRedrawScreen = true;
 
       gotF(scanF);
-      SVC_FC_Init();
+      return;
     }
   } else {
     if (hits) {
@@ -76,16 +84,15 @@ void SVC_FC_Update(void) {
   }
   BK4819_DisableFrequencyScan();
   BK4819_EnableFrequencyScanEx(T);
+  scanning = true;
   uint32_t bound = SETTINGS_GetFilterBound();
   if (f && ((lower && f < bound) || (!lower && f >= bound))) {
     scanF = f;
-    Log("f: %u", f);
   }
 }
 
 void SVC_FC_Deinit(void) {
+  scanning = false;
   BK4819_StopScan();
-  BK4819_RX_TurnOn();
-  RADIO_SetupByCurrentVFO();
   SVC_Toggle(SVC_LISTEN, true, 10);
 }
