811e267bb3de59770e9cf4270690b2b176c65136
iphizic <iphizic@ya.ru>
2024-11-24 15:54:23 +0300

    Fix border frequency

--- src/radio.c
+++ src/radio.c
@@ -86,7 +86,7 @@ static const SI47XX_FilterBW SI_BW_MAP_AMFM[] = {
 void RADIO_HasSi() { hasSI = BK1080_ReadRegister(1) != 0x1080; }
 
 Radio RADIO_Selector(uint32_t freq) {
-  if (freq > 30000000 ) {
+  if (freq > 3000000 ) {
     return RADIO_BK4819;
   }
   return hasSI ? RADIO_SI4732 : RADIO_BK1080;
